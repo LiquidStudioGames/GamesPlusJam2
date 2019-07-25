9f104c756c544390344f5fe79d6a8f2f858fef91
Daniel Munch <daniellmunch@gmail.com>
2019-07-25 23:42:32 +0200

    Added menu basics
    I'll be doing graphics and level loading soon

---
+++ PlanetaryRouting/Assets/Scripts/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+  using UnityEditor;
+#endif
+
+public class Menu : MonoBehaviour
+{
+    void LoadData ()
+    {
+        // Load the json file containing all the players data
+    }
+
+    void LoadLevel (string LevelName)
+    {
+        // Load a level by it's name
+    }
+
+    public void StartClick ()
+    {
+        // Check which level the player has reached, and load it
+    }
+
+    public void QuitClick ()
+    {
+      #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode ();
+      #else
+        Application.Quit ();
+      #endif
+    }
+}
--- PlanetaryRouting/Assets/Scripts/Rocket.cs
+++ PlanetaryRouting/Assets/Scripts/Rocket.cs
@@ -6,60 +6,60 @@ public class Rocket : MonoBehaviour
     public float staminaUsage = 100f;
     public float rotation = 20f;
     public float acceleration = 100f;
-    
+
     private bool launched;
     private Rigidbody rb;
 
-    private void Start()
+    private void Start ()
     {
         launched = false;
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody> ();
     }
 
-    private void Update()
+    private void Update ()
     {
         if (!launched)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown (KeyCode.Space))
                 launched = true;
         }
 
         else if (stamina > 0f)
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey (KeyCode.Space))
             {
                 stamina -= staminaUsage * Time.deltaTime;
-                rb.AddForce(transform.up * acceleration * Time.deltaTime, ForceMode.Acceleration);
+                rb.AddForce (transform.up * acceleration * Time.deltaTime, ForceMode.Acceleration);
             }
 
-            if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+            if (Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl))
             {
                 stamina -= staminaUsage * Time.deltaTime;
-                rb.AddForce(-transform.up * acceleration * Time.deltaTime, ForceMode.Acceleration);
+                rb.AddForce (-transform.up * acceleration * Time.deltaTime, ForceMode.Acceleration);
             }
 
             if (Input.GetKey(KeyCode.A))
             {
                 stamina -= staminaUsage * Time.deltaTime;
-                AddRotation(Quaternion.Euler(0f, 0f, rotation * Time.deltaTime));
+                AddRotation (Quaternion.Euler (0f, 0f, rotation * Time.deltaTime));
             }
 
             if (Input.GetKey(KeyCode.D))
             {
                 stamina -= staminaUsage * Time.deltaTime;
-                AddRotation(Quaternion.Euler(0f, 0f, -rotation * Time.deltaTime));
+                AddRotation (Quaternion.Euler (0f, 0f, -rotation * Time.deltaTime));
             }
         }
     }
 
-    public void AddRotation(Quaternion rotation)
+    public void AddRotation (Quaternion rotation)
     {
         transform.rotation *= rotation;
         rb.velocity = rotation * rb.velocity;
     }
 
-    public void AddForce(Vector3 force)
+    public void AddForce (Vector3 force)
     {
-        transform.Translate(force, Space.World);
+        transform.Translate (force, Space.World);
     }
 }
